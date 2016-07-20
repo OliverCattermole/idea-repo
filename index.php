2cbc02e3f2b213bdd455838f1f0ecd6694623184
Tom Griffin <idanteusa@gmail.com>
2016-07-20 10:42:05 +0100

    Openshift

--- index.php
+++ index.php
@@ -1,78 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+/**
+ * Requests collector.
+ *
+ *  This file collects requests if:
+ *	- no mod_rewrite is available or .htaccess files are not supported
+ *  - requires App.baseUrl to be uncommented in app/Config/core.php
+ *	- app/webroot is not set as a document root.
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c), Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 
-    <title>Idea Repo</title>
+/**
+ *  Get CakePHP's root directory
+ */
+define('APP_DIR', 'app');
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
+define('WEBROOT_DIR', 'webroot');
+define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
 
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://bootflat.github.io/bootflat/css/bootflat.css">
-    <link rel="stylesheet" href="libraries/font-awesome-4.6.3/css/font-awesome.min.css">
-    <link rel="stylesheet" href="style.css">
-</head>
+/**
+ * This only needs to be changed if the "cake" directory is located
+ * outside of the distributed structure.
+ * Full path to the directory containing "cake". Do not add trailing directory separator
+ */
+if (!defined('CAKE_CORE_INCLUDE_PATH')) {
+	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
+}
 
-<body>
-    <div class="container-fluid">
-        <div class="spacer"></div>
-
-		<div class="row">
-			<div class="col-lg-3 col-md-2 col-sm-1"></div>
-			<div class="col-lg-6 col-md-8 col-sm-10">
-			   <form class="form-horizontal">
-                  <div class="form-group">
-
-                    <label class="col-sm-2 control-label">Title</label>
-                    <div class="col-sm-10">
-                      <input type="text" class="form-control"  placeholder="Title">
-                    </div>
-                  </div>
-
-                  <div class="form-group">
-                    <label class="col-sm-2 control-label">Description</label>
-                    <div class="col-sm-10">
-                        <textarea class="form-control" rows="5" placeholder="Description"></textarea>      
-                    </div>
-                  </div>
-
-                  <div class="form-group">
-                    <label class="col-sm-2 control-label">Category</label>
-                    <div class="col-sm-5">
-                      <select class="form-control">
-                          <option>1</option>
-                          <option>2</option>
-                          <option>3</option>
-                          <option>4</option>
-                          <option>5</option>
-                        </select>
-                    </div>
-                  </div>
-
-                  <div class="form-group">
-                    <label class="col-sm-2 control-label">Useful links</label>
-                    <div class="col-sm-10">
-                        <textarea class="form-control" rows="5" placeholder="Add links to further reading materials here"></textarea>      
-                    </div>
-                  </div>
-
-                  <button type="submit" class="btn btn-primary pull-right">Submit</button>
-
-                </form>
-			</div>
-			<div class="col-lg-3 col-md-2 col-sm-1"></div>
-		</div>
-	    
-	 
-
-
-    <!-- Bootstrap -->
-    <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-
-    <!-- Bootflat's JS files.-->
-    <script src="https://bootflat.github.io/bootflat/js/icheck.min.js"></script>
-    <script src="https://bootflat.github.io/bootflat/js/jquery.fs.selecter.min.js"></script>
-    <script src="https://bootflat.github.io/bootflat/js/jquery.fs.stepper.min.js"></script>
-
-</body>
-</html>
+require APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
