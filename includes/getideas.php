d63a2f9256e947d7468886638dfe33eb6d83e53e
Matt Dean <matt.dean.001@gmail.com>
2016-08-02 11:30:49 +0100

    changed db credentials

--- includes/getideas.php
+++ includes/getideas.php
@@ -11,7 +11,7 @@
 		$stmt = $db->query('SELECT * FROM ideas ORDER BY pk_id DESC');
 		$ideas = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}else{
-		$db = new PDO('mysql:host=b023a08296a1c5:3a6c2a33@eu-cdbr-west-01.cleardb.com/heroku_f3ca2ad785fc4af;dbname=idearepo;', 'b023a08296a1c5', '3a6c2a33');
+		$db = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_f3ca2ad785fc4af;', 'b023a08296a1c5', '3a6c2a33');
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
