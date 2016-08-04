2043ec9be86fa5ec5a82b28a627fc6d090573531
Matt Dean <matt.dean.001@gmail.com>
2016-08-04 08:46:21 +0100

    try db connection using environment variables

--- includes/setupdbconn.php
+++ includes/setupdbconn.php
@@ -1,6 +1,8 @@
 <?php
-	//set up db connection		
-	$db = new PDO('mysql:host=mysql;dbname=idearepo;', 'userILI', 'Og4qySXlYk2IICln');
+	//set up db connection
+	define('DB_USER', getenv('MYSQL_USER')); // admin
+	define('DB_PASS', getenv('MYSQL_PASSWORD')); 	
+	$db = new PDO('mysql:host=mysql;dbname=idearepo;', DB_USER, DB_PASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 ?>
