f0c3d6cc45adc88fa1a418c8c475648c86e3dc5a
Matt Dean <matt.dean.001@gmail.com>
2016-08-04 08:04:29 +0100

    removed some unneeded files from master and changed db connection

--- includes/setupdbconn.php
+++ includes/setupdbconn.php
@@ -1,6 +1,13 @@
 <?php
 	//set up db connection		
-	$db = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_f3ca2ad785fc4af;', 'b023a08296a1c5', '3a6c2a33');
+	define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+
+$dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+$db = new PDO($dsn, DB_USER, DB_PASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 ?>
