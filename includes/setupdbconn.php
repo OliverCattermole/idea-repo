aab67f335080cb556b789af9ec18a972ae595848
Matt Dean <matt.dean.001@gmail.com>
2016-08-04 08:11:14 +0100

    changed db connection

--- includes/setupdbconn.php
+++ includes/setupdbconn.php
@@ -1,6 +1,6 @@
 <?php
 	//set up db connection		
-	$db = new PDO('mysql:host=172.30.70.84;dbname=idearepo;', 'userILI', 'Og4qySXlYk2IICln');
+	$db = new PDO('mysql:host=mysql;dbname=idearepo;', 'userILI', 'Og4qySXlYk2IICln');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 ?>
