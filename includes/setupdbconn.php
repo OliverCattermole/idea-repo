28244317eec880ec1422d8def56ee99f99093f67
Matt Dean <matt.dean.001@gmail.com>
2016-08-04 08:58:15 +0100

    try db connection using environment variables

--- includes/setupdbconn.php
+++ includes/setupdbconn.php
@@ -1,7 +1,7 @@
 <?php
 	//set up db connection
-	define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME')); // admin
-	define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD')); 	
+	define('DB_USER', getenv('MYSQL_USER')); // admin
+	define('DB_PASS', getenv('MYSQL_PASSWORD')); 	
 	$db = new PDO('mysql:host=mysql;dbname=idearepo;', DB_USER, DB_PASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
