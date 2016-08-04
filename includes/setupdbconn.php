a5385621721e6e8f28a54d3bf659a04304024bf2
Matt Dean <matt.dean.001@gmail.com>
2016-08-04 08:09:53 +0100

    changed db connection

--- includes/setupdbconn.php
+++ includes/setupdbconn.php
@@ -1,15 +1,6 @@
 <?php
 	//set up db connection		
-	define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
-define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
-
-echo DB_NAME;
-
-$dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
-$dbname = new PDO($dsn, DB_USER, DB_PASS);
+	$db = new PDO('mysql:host=172.30.70.84;dbname=idearepo;', 'userILI', 'Og4qySXlYk2IICln');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 ?>
