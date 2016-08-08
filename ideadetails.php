0d217913201b6afa7848d64ea367b0e0f17830ab
Matt Dean <matt.dean.001@gmail.com>
2016-08-08 10:59:06 +0100

    added dbconn to gitignore and trialling htmlcommentbox on ideadetails

--- ideadetails.php
+++ ideadetails.php
@@ -121,6 +121,12 @@
 			<div class="col-lg-3 col-md-2 col-sm-1"></div>
 		</div>
 
+<!-- begin wwww.htmlcommentbox.com -->
+ <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
+ <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+ <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24zhBxO8EDPT8JnlTCSp7Fj1"+"&opts=16862&num=10&ts=1470649427860");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+<!-- end www.htmlcommentbox.com -->
+
 	<?php include 'includes/importscripts.php' ?>
 
 
--- includes/setupdbconn.php
+++ includes/setupdbconn.php
@@ -1,6 +1,6 @@
 <?php
 	//set up db connection		
-	$db = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_f3ca2ad785fc4af;', 'b023a08296a1c5', '3a6c2a33');
+	$db = new PDO('mysql:host=127.0.0.1;dbname=idea-repo;', 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 ?>
