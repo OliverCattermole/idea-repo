718b1bb9a39ba33b13ed9c9f220a0945157d510d
Matt Dean <matt.dean.001@gmail.com>
2016-08-10 10:24:45 +0100

    added sbld header

--- ideadetails.php
+++ ideadetails.php
@@ -44,7 +44,7 @@
 
 <body>
     <div class="container-fluid">
-    
+    <?php include 'includes/sitebuilderheader.php'; ?>
     <div class="row">
     	<div class="col-lg-3 col-md-2 col-sm-1"></div>
     	<a class="btn btn-success expandbutton"
