1c2987b1a053ad015a141f9a90e7e0dcffb56941
Matt Dean <matt.dean.001@gmail.com>
2016-08-10 10:20:40 +0100

    small style changes

---
+++ includes/sitebuilderheader.php
@@ -0,0 +1,87 @@
+<div class="header-navbar-fixed header-navbar-dark">
+   <span id="j_id0:j_id1:j_id6">
+    <header class="content-mini content-mini-full" id="header-navbar">
+        <ul class="nav-header pull-right">
+            <li>
+                <div class="btn-group">
+                    <button class="btn btn-default btn-image dropdown-toggle" data-toggle="dropdown" type="button"><img src="https://gso--c.eu2.visual.force.com/resource/1470673703000/GLBL_SBLD_Images/assets/img/avatar.jpg" alt="Avatar">
+                        <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li class="dropdown-header">Profile</li>
+                        <li>
+                            <a href="https://gso--c.eu2.visual.force.com/apex/GLBL_SBLD_Profile?id=a7Db0000000CahtEAC" tabindex="-1">
+                                <i class="si si-user pull-right"></i>Profile
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://gso--c.eu2.visual.force.com/ui/setup/Setup?setupid=PersonalSetup" tabindex="-1">
+                                <i class="si si-settings pull-right"></i>Settings
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li class="dropdown-header">Actions</li>
+                        <li>
+                            <a href="/" tabindex="-1">
+                                <i class="si si-logout pull-right"></i>Exit
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="hidden-md hidden-lg">
+                <button class="btn btn-link text-white pull-right" data-class="nav-main-header-o" data-target=".js-nav-main-header" data-toggle="class-toggle" type="button">
+                    <i class="fa fa-navicon"></i>
+                </button>
+            </li>
+        </ul>
+        <ul class="js-nav-main-header nav-main-header pull-right">
+            <li class="text-right hidden-md hidden-lg">
+                <button class="btn btn-link text-white" data-class="nav-main-header-o" data-target=".js-nav-main-header" data-toggle="class-toggle" type="button">
+                    <i class="fa fa-times"></i>
+                </button>
+            </li><li>
+            <a href="https://gso--c.eu2.visual.force.com/apex/GLBL_SBLD_Home?id=a7Db0000000Caht">Home</a>
+        </li>
+
+        <li>
+            <a class="nav-submenu" href="https://gso--c.eu2.visual.force.com/apex/GLBL_SBLD_Page_02?id=a7Db0000000Caht&content=a7Ab0000000TNHN">About Us</a>
+            <ul>
+                <li>
+                    <a href="https://gso--c.eu2.visual.force.com/apex/GLBL_SBLD_Page_02?id=a7Db0000000Caht&content=a7Ab0000000TNHN">Goals/Scope</a>
+                </li>
+                <li>
+                    <a href="https://gso--c.eu2.visual.force.com/apex/GLBL_SBLD_Team_Section?id=a7Db0000000Caht">Team Members</a>
+                </li>
+            </ul>
+        </li>
+
+        <li>
+            <a href="https://lctportal-idea-repo.herokuapp.com/">Forum</a>
+        </li>
+
+        <li>
+            <a href="https://gso--c.eu2.visual.force.com/apex/GLBL_SBLD_Blog_List?id=a7Db0000000Caht">Blog</a>
+        </li>
+
+        <li>
+            <a class="nav-submenu" href="https://gso--c.eu2.visual.force.com/apex/GLBL_SBLD_Release_List?id=a7Db0000000Caht">Core (Private)</a>
+            <ul>
+                <li>
+                    <a href="https://gso--c.eu2.visual.force.com/apex/GLBL_SBLD_Release_List?id=a7Db0000000Caht">Updates</a>
+                </li>
+                <li>
+                    <a href="https://gso--c.eu2.visual.force.com/apex/GLBL_SBLD_Page_01?id=a7Db0000000Caht&amp;content=a7Ab0000000GmmK">Document Library</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <ul class="nav-header pull-left">
+        <li class="header-content">
+            <a class="h5" href="#"><img src="https://gso--c.eu2.visual.force.com/resource/1470673703000/GLBL_SBLD_Images/assets/img/lillylogo_white.png" alt="Lilly Logo" height="35">
+            </a>
+        </li>
+    </ul>
+</header>
+</span>
+</div>
