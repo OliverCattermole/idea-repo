2ffef0fe85a0b2b8cd97ae4e3ea7151d3da88bcf
Matt Dean <matt.dean.001@gmail.com>
2016-09-28 10:13:48 +0100

    Update setupdbconn.php

--- includes/setupdbconn.php
+++ includes/setupdbconn.php
@@ -1,6 +1,6 @@
 <?php
   	//set up db connection	
-$db = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_6563eadbc9d3d21;', 'bce3bc60ab26b1', 'df942bc2');
+$db = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_fb533df36dc57ba;', 'be1e449a1084cc', '608e9dd5');
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);		     
       $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 ?>
